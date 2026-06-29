cebad936f5843a3fad48d667e4dbe63e1c7a9171
Vantix <117606560+AstroMods@users.noreply.github.com>
2026-06-29 12:45:42 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    stationName: "Vantix Radio",
+    stationSubtitle: "The Official Radio Station of Andreas Initiative Roleplay",
+
+    streamUrl: "http://51.255.235.165:2128/stream.mp3",
+
+    logo: "assets/logo.png",
+    favicon: "assets/favicon.ico",
+
+    discordInvite: "https://discord.com/invite/YaaEnHTSvk",
+
+    autoPlayOnInteraction: true
+};
